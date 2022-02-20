958055b1d95f5529a9c9ded5be8685b0ac39dd3b
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-02-19 20:48:00 -0500

    Completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+
+20.times do 
+    Coupon.create(coupon_code: Faker::Commerce.promotion_code, store: Faker::Commerce.brand)
+end
